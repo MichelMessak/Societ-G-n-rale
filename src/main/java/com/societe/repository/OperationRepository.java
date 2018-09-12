c1d00703d6df5742acf2bd1e400d608e72a56d49
Michel Messak <mmessak@ippon.fr>
2018-09-12 15:45:28 +0200

    Fix Operation repository javadoc

--- src/main/java/com/societe/repository/OperationRepository.java
+++ src/main/java/com/societe/repository/OperationRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Repository for operation
+ */
 @Repository
 public interface OperationRepository extends JpaRepository<Operation, Long> {
 
